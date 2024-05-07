a81972db2b97bad04024ed576758c0a7329b0412
Christian Noss <christian.noss@th-koeln.de>
2024-05-07 20:28:16 +0200

    Added results

--- assets/scripts/modules/keyboard-navigation.js
+++ assets/scripts/modules/keyboard-navigation.js
@@ -3,6 +3,39 @@
 
 const addKeyboardEvents = () => {
   
+  const contentWrap = document.querySelector("[data-js-content]");
+  const navObjects = document.querySelectorAll("[data-js-content] > *");
+  const navObjectsArray = Array.from(navObjects);
+  let currentNavObject = 0;
+
+  const jump = (direction) => {
+    if (direction === "up") {
+      currentNavObject = currentNavObject === 0 ? 0 : currentNavObject - 1;
+    } else if (direction === "down") {
+      currentNavObject = currentNavObject === navObjectsArray.length ? currentNavObject : currentNavObject + 1;
+    }
+
+    navObjectsArray[currentNavObject].scrollIntoView({ behavior: "smooth" });
+    history.pushState("", "", `#${navObjectsArray[currentNavObject].id}`);
+  };
+
+
+  document.onkeydown = function (event) {
+
+    switch (event.keyCode) {
+      case 38:
+        jump("up");
+        break;
+
+      case 40:
+        jump("down");
+        break;
+
+      case 79:
+        contentWrap.classList.toggle("is-overview");
+        break;
+    }
+  };
 };
 
 
--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,7 +2,22 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
+  const observedElements = document.querySelectorAll('[data-js-observe]');
+  const options = { rootMargin: "0px", threshold: 0.6 };
 
+  const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-in-view");
+      } else {
+        entry.target.classList.remove("is-in-view");
+      }
+    });
+  }, options);
+
+  observedElements.forEach(function (element) {
+    observer.observe(element);
+  });
 }
 
 
